d62746f85f3b47f3c4bac7ab7c49a99d5bffeb1a
James Prentice <66943799+JamesPrenticez@users.noreply.github.com>
2023-07-26 13:58:01 +1200

    Initial commit

---
+++ src/constants.tsx
@@ -0,0 +1,8 @@
+export const project = {
+  name: "BOILER PLATE",
+};
+
+export const pages = [
+  { name: "HOME", url: "/", requiresAuth: false },
+  { name: "ABOUT", url: "/about", requiresAuth: false },
+];
