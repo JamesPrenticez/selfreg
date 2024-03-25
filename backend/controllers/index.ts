14fba2e1d3ff096509f906139df974a253667cbb
James Prentice <jamesprenticez@gmail.com>
2024-03-25 17:44:34 +1300

    BE - getting connected to prod db

--- backend/controllers/index.ts
+++ backend/controllers/index.ts
@@ -1,7 +1,3 @@
-// const { id } = req.params;
-// const { id } = req.query;
-// const { id } = req.body;
-
 export * from './authControllers'
 export * from './userControllers'
 export * from './habitsControllers'
